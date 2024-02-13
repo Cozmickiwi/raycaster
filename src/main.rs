94690bc9d2f87608719788806be88244c010c30e
Cozmickiwi <sam.rogers.kiwi@gmail.com>
2024-02-14 02:52:35 +1300

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use std::{cmp::min, f32::consts::PI};
 
-use pixels::{wgpu::Color, Pixels, SurfaceTexture};
+use pixels::{Pixels, SurfaceTexture};
 use winit::{
     dpi::{LogicalSize, PhysicalSize},
     event::{Event, WindowEvent},
     event_loop::EventLoop,
-    window::{Window, WindowBuilder},
+    window::WindowBuilder,
 };
 
 struct Raycaster {
@@ -36,7 +36,7 @@ const MAP: [[u8; 10]; 10] = [
 
 const SCREEN_WIDTH: u16 = 1500;
 const SCREEN_HEIGHT: u16 = 900;
-const HALF_HEIGHT: u16 = 450;
+//const HALF_HEIGHT: u16 = 450;
 const SCALE: u16 = 1;
 
 fn main() {
@@ -58,36 +58,18 @@ fn main() {
         .unwrap();
     event_loop.set_control_flow(winit::event_loop::ControlFlow::Poll);
     let window_size = builder.inner_size();
-
-    /*
-    for pixel in frame.chunks_exact_mut(4) {
-        pixel[0] = 255; // R
-        pixel[1] = 27; // G
-        pixel[2] = 71; // B
-        pixel[3] = 0xff; // A
-    }*/
-    //draw_square(&mut frame, &window_size, 500, 400, 200, 200);
-    //draw_square(&mut frame, &window_size, 300, 150, 100, 50);
-    //    raycast(&player, &mut raycaster, frame, &window_size);
     let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &builder);
     let mut pixels = Pixels::new(window_size.width, window_size.height, surface_texture).unwrap();
-    //    pixels.clear_color(Color::BLUE);
-    //pixels.clear_color(Color::RED);
-
     for pixel in pixels.frame_mut().chunks_exact_mut(4) {
         pixel[0] = 255; // R
         pixel[1] = 27; // G
         pixel[2] = 71; // B
         pixel[3] = 0xff; // A
     }
-    //
-
-    //            let mut frame = pixels.frame_mut();
     let mut wasd: [bool; 4] = [false, false, false, false];
     event_loop
         .run(move |event, elwt| {
             raycast(&player, &mut raycaster, pixels.frame_mut(), &window_size);
-
             pixels.render().unwrap();
             match event {
                 Event::WindowEvent {
@@ -98,33 +80,19 @@ fn main() {
                     elwt.exit();
                 }
                 Event::AboutToWait => {
-                    // Application update code.
-
-                    // Queue a RedrawRequested event.
-                    //
-                    // You only need to call this if you've determined that you need to redraw in
-                    // applications which do not always need to. Applications that redraw continuously
-                    // can render here instead.
                     builder.request_redraw();
                 }
                 Event::WindowEvent {
                     event: WindowEvent::RedrawRequested,
                     ..
                 } => {
-                    // Application update code.
-
-                    // Queue a RedrawRequested event.
-                    //
-                    // You only need to call this if you've determined that you need to redraw in
-                    // applications which do not always need to. Applications that redraw continuously
-                    // can render here instead.
                     builder.request_redraw();
                 }
                 Event::WindowEvent { event, .. } => match event {
                     WindowEvent::KeyboardInput {
-                        device_id,
+                        device_id: _,
                         event,
-                        is_synthetic,
+                        is_synthetic: _,
                     } => match event.physical_key {
                         winit::keyboard::PhysicalKey::Code(winit::keyboard::KeyCode::KeyA) => {
                             if event.state.is_pressed() {
@@ -154,7 +122,7 @@ fn main() {
                                 wasd[2] = false
                             }
                         }
-                        _ => {},
+                        _ => {}
                     },
                     _ => {}
                 },
@@ -186,7 +154,7 @@ fn main() {
         .unwrap();
 }
 
-const TESTCOLOR: [u8; 4] = [0, 27, 71, 0];
+//const TESTCOLOR: [u8; 4] = [0, 27, 71, 0];
 const BLUE1: [u8; 4] = [25, 122, 154, 0];
 const PURPLE1: [u8; 4] = [131, 60, 169, 0];
 const RED1: [u8; 4] = [154, 25, 70, 0];
@@ -214,12 +182,6 @@ fn draw_square(
                 i[1] = color[1];
                 i[2] = color[2];
             }
-            /*
-            frame[pixel_index] = color[0];
-            frame[pixel_index + 1] = color[1];
-            frame[pixel_index + 2] = color[2];
-            frame[pixel_index + 3] = color[3];
-            */
         }
     }
 }
@@ -233,7 +195,6 @@ fn raycast(
     player: &Player,
     ray: &mut Raycaster,
     frame: &mut [u8],
-    //    pixels: &mut Pixels,
     window_size: &PhysicalSize<u32>,
 ) {
     let mut ray_angle = player.angle as f32 - player.half_fov as f32;
@@ -248,7 +209,6 @@ fn raycast(
         let pres = ray.precision as f32;
         let ray_cos = ray_rad.cos() / pres;
         let ray_sin = ray_rad.sin() / pres;
-
         // Wall check
         let mut wall = 0;
         while wall == 0 {
@@ -259,14 +219,14 @@ fn raycast(
         let mut distance =
             ((player.x - ray_struct.x).powi(2) + (player.y - ray_struct.y).powi(2)).sqrt();
         let panglef = ray_angle - player.angle as f32;
-        // wall height
+        // Wall height
         distance = distance * (panglef.to_radians()).cos();
         let wall_height = (window_size.height / 2) as f32 / distance;
         let mut wall_start = window_size.height / 2 - wall_height as u32;
         if distance <= 1.0 {
             wall_start = 1;
         }
-        let true_height = wall_height as u32 * 2;
+        //        let true_height = wall_height as u32 * 2;
         draw_square(
             frame,
             window_size,
